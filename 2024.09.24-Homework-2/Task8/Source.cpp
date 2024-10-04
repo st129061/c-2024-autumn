db4356bc5a1ec7bc41f1cccc6c308362984ad350
st129061 <aromirmonkevic@gmail.com>
2024-10-04 23:19:14 +0300

    Homework 2 Task 8 complite

--- 2024.09.24-Homework-2/Task8/Source.cpp
+++ 2024.09.24-Homework-2/Task8/Source.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include <cmath>
+
+// 1 1 4    1 * 2 + 2 * 1 + 1 - 1 + 1 - 1
+// 2 2 16   2 * 3 + 2 * 3 + 2 + 2
+// 4 3 38   4 * 4 + 3 * 5 + 4 + 3
+// 3 3 28   3 * 4 + 3 * 4 + 3 - 1 + 3 - 1
+// 4 4 48   4 * 5 + 4 * 5 + 4 + 4
+// 5 3 44   5 * 4 + 3 * 6 + 5 + 3 - 1 - 1
+// 5 5 68   5 * 6 + 5 * 6 + 5 - 1 + 5 - 1
+// 
+//
 
 int main( int argc, char * argv[] )
 {
-  int M = 0;
-  int N = 0;
+  int n = 0;
+  int m = 0;
 
-  scanf_s("%d", &M);
-  scanf_s("%d", &N);
+  scanf_s("%d", &n);
+  scanf_s("%d", &m);
   
-  int a = std::min(M / 2, N / 2);
-  int newM = M - 2 * a;
-  int newN = N - 2 * a;
-  int add = 0;
-  if (newM == 0 || newN == 0)
-    add = (newM + newM) * 2;
-  printf("%d", M * N * 4 + add);
+  if (n == 1 || m == 1 || n % 2 == 1 && m % 2 == 1)
+  {
+    printf("%d", n * (m + 1) + m * (n + 1) + n + m - 2);
+  }
+  else
+  {
+    printf("%d", n * (m + 1) + m * (n + 1) + n + m);
+  }
 
   return EXIT_SUCCESS;
 }
